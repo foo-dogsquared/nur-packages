a2b44775c2e04e0f8cbb354073790eeb352476b3
Gabriel Arazas <foo.dogsquared@gmail.com>
2020-09-04 12:18:51 +0800

    Correct the Hypermail package

--- pkgs/hypermail.nix
+++ pkgs/hypermail.nix
@@ -6,8 +6,16 @@ stdenv.mkDerivation rec {
 
   src = fetchFromGitHub {
     owner = "hypermail-project";
-    repo = "${pname}"
+    repo = "${pname}";
     rev = "v2.4.0";
     sha256 = stdenv.lib.fakeSha256;
+  };
+
+  meta = with stdenv.lib; {
+    description = "A converter for emails from Unix mbox format.";
+    homepage = "http://www.hypermail-project.org/";
+    license = licenses.gpl;
+    maintainers = [ maintainers.foo-dogsquared ];
+    platforms = [ "i686-linux" "x86_64-linux" ];
   };
 }
