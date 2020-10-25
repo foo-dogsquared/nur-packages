4865289e2ecaaef24e6482e6c915a38a8c69ad59
Gabriel Arazas <foo.dogsquared@gmail.com>
2020-10-26 07:18:36 +0800

    wikiman: update to 2.12.1

--- pkgs/wikiman/default.nix
+++ pkgs/wikiman/default.nix
@@ -1,31 +1,35 @@
-{ stdenv, fetchFromGitHub,
-findutils,
-fzf,
-gawk,
-ripgrep,
-w3m,
-}:
+{ stdenv, fetchFromGitHub, callPackage, findutils, fzf, gawk, ripgrep, w3m
+, installShellFiles,
 
-stdenv.mkDerivation rec {
+# Make a parameter for enabling each source.
+enableArchSource ? false, enableGentooSource ? false
+, enableFreeBSDSource ? false, enableTLDRSource ? false, }:
+
+let
+  wikiman-sources = callPackage ./wikiman-sources.nix { };
+  installSources = cond: pkg:
+    stdenv.lib.optionalString cond
+    "mkdir -p $out/share/doc/ && cp --recursive ${pkg}/share/doc/* $out/share/doc";
+in stdenv.mkDerivation rec {
   pname = "wikiman";
-  version = "2.11.1";
+  version = "2.12.1";
 
   src = fetchFromGitHub {
     owner = "filiparag";
     repo = "wikiman";
     rev = "${version}";
-    sha256 = "01ir6q5iwxms01l2j3md10ixfb06gjb42d9sjr665qhy0rh9sgr5";
+    sha256 = "sha256-+TTjzD3qkvSsaXgHEgHKGpNqkEs3ERNCsFzlvonNhRY=";
   };
 
-  patches = [
-    ./wikiman-nix.patch
-  ];
-  postPatch= ''
+  patches = [ ./wikiman-nix.patch ];
+
+  postPatch = ''
     substituteInPlace wikiman.sh --replace "@INSTALL_DIR@" $out
     substituteInPlace Makefile --replace "@INSTALL_DIR@" $out
   '';
 
-  buildInputs = [ fzf gawk ripgrep w3m ];
+  nativeBuildInputs = [ fzf gawk ripgrep w3m installShellFiles ];
+
   installPhase = ''
     mkdir -p $out/bin \
              $out/share/${pname} \
@@ -37,13 +41,21 @@ stdenv.mkDerivation rec {
     cp -fr sources $out/share/${pname}
     cp -fr widgets $out/share/${pname}
     install -Dm644 LICENSE $out/share/licenses/${pname}
-    install -Dm644 wikiman.conf $out/share/wikiman/wikiman.conf
-    install -Dm644 README.md $out/share/doc/wikiman
-    tar -czf $out/man/man1/${pname}.1.gz ./${pname}.1.man
+    install -Dm644 wikiman.conf $out/share/${pname}/wikiman.conf
+    install -Dm644 README.md $out/share/doc/${pname}
+
+    installManPage wikiman.1.man
+    installShellCompletion completions/completions.{bash,fish,zsh}
+
+    ${installSources enableArchSource wikiman-sources.arch}
+    ${installSources enableGentooSource wikiman-sources.gentoo}
+    ${installSources enableFreeBSDSource wikiman-sources.freebsd}
+    ${installSources enableTLDRSource wikiman-sources.tldr}
   '';
 
   meta = with stdenv.lib; {
-    description = "A command-line interface for searching between documentation sources such as Arch Wiki, Gentoo Wiki, etc.";
+    description =
+      "A command-line interface for searching between documentation sources such as Arch Wiki, Gentoo Wiki, etc.";
     homepage = "https://github.com/filiparag/wikiman";
     license = licenses.mit;
     maintainers = [ maintainers.foo-dogsquared ];
