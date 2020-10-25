5e9722e0ca8a1c6194b2c4918bccb0672528efd2
Gabriel Arazas <foo.dogsquared@gmail.com>
2020-10-26 03:15:29 +0800

    julia-bin: init at 1.5.2

--- default.nix
+++ default.nix
@@ -18,6 +18,7 @@
   defold = pkgs.callPackage ./pkgs/defold.nix { };
   edir = pkgs.callPackage ./pkgs/edir.nix { };
   hypermail = pkgs.callPackage ./pkgs/hypermail.nix { };
+  julia-bin = pkgs.callPackage ./pkgs/julia-bin.nix { };
   openring = pkgs.callPackage ./pkgs/openring.nix {  };
   segno = pkgs.callPackage ./pkgs/segno.nix { };
   tic-80 = pkgs.callPackage ./pkgs/tic-80.nix { };
---
+++ pkgs/julia-bin.nix
@@ -0,0 +1,31 @@
+# The nixpkgs of Julia is not yet successfully updated (or even built).
+# See https://github.com/NixOS/nixpkgs/issues/91930 and https://github.com/NixOS/nixpkgs/pull/98042 to know the status of how Julia is built on the package set.
+# For now, I'll be packaging the binary since compilation is too long.
+{ stdenv, fetchurl, autoPatchelfHook }:
+
+stdenv.mkDerivation rec {
+  pname = "julia-bin";
+  version = "1.5.2";
+
+  src = fetchurl {
+    url = "https://julialang-s3.julialang.org/bin/linux/x64/1.5/julia-1.5.2-linux-x86_64.tar.gz";
+    sha256 = "sha256-bacE+tzvo5clUD5Mepz6GlcLqKZHxL2N5poRj0NYRjA=";
+  };
+
+  nativeBuildInputs = [
+    autoPatchelfHook
+  ];
+
+  installPhase = ''
+    mkdir $out
+    cp -R * $out/
+  '';
+
+  meta = with stdenv.lib; {
+    description = "The binary build for Julia 1.5 on 64-bit Linux systems.";
+    homepage = https://julialang.org/;
+    license = licenses.free;
+    maintainers = with maintainers; [ foo-dogsquared ];
+    platforms = [ "x86_64-linux" ];
+  };
+}
